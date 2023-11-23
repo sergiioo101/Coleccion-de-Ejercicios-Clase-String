975abc2684b68c42af9f67df9520e8452c6216ab
sergiioo101 <smartros@myuax.com>
2023-11-23 12:18:06 +0100

    ejercicio2

--- ejercicio2/main.cpp
+++ ejercicio2/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <string>
 
+// Función para imprimir el tamaño de la cadena y la cadena misma
+void imprimirTamanoYCadena(const std::string& str) {
+    std::cout << "Tamaño de la cadena: " << str.size() << std::endl;
+}
+
+// Función para verificar si la cadena está vacía y mostrar la cadena
+void verificarCadenaVaciaYMostrar(const std::string& str) {
+    std::cout << "¿Esta la cadena vacia? " << (str.empty() ? "Sí" : "No") << std::endl;
+    std::cout << "Cadena: " << str << std::endl;
+}
+
 int main() {
-    std::string sentence = "OpenAI is a leading AI research lab.";
-    std::cout << "Tamaño de la cadena: " << sentence.size() << std::endl;
-    std::cout << "¿Está la cadena vacía? " << (sentence.empty() ? "Sí" : "No") << std::endl;
+    std::string sentence = "Ruben es el mejor profesor de la UAX";
+
+    // Llamar a las funciones mejoradas
+    imprimirTamanoYCadena(sentence);
+    verificarCadenaVaciaYMostrar(sentence);
+
     return 0;
 }
+
